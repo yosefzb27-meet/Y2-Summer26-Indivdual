125ded31a6c97cbce065aac0be57be0d98619542
yosefzb27-meet <zbedatyousif@gmail.com>
2026-07-13 19:56:05 +0300

    Yosef
    submission lab1

---
+++ Agent/app.py
@@ -0,0 +1,38 @@
+import os
+from anthropic import Anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Anthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
+
+def run_chat():
+    print('You: (type exit to quit)')
+    system_message = """
+You are a motivational coach.
+Always encourage the user.
+Keep your answers positive and inspiring.
+"""
+    history = []
+
+    while True:
+        user_input = input('>> ')
+
+        if user_input.lower() == 'exit':
+            break
+
+        history.append({'role': 'user', 'content': user_input})
+
+        response = client.messages.create(
+            model='claude-haiku-4-5-20251001',
+            max_tokens=300,
+            temperature=0.7,
+            system=system_message,
+            messages=history
+        )
+
+        reply = response.content[0].text
+        print(f'Claude: {reply}')
+        history.append({'role': 'assistant', 'content': reply})
+
+run_chat()
